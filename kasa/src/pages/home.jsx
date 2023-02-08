fb3092fca344b89d0fad552a4b841fc52c1343ae
Iboow <luc.tiramani@gmail.com>
2023-02-08 10:32:25 +0100

    banner

--- kasa/src/pages/home.jsx
+++ kasa/src/pages/home.jsx
@@ -1,12 +1,24 @@
 import Header from "../Components/header";
 import Footer from "../Components/footer";
+import Banner from "../Components/banner";
+import accomodationList from "../accomodationList";
 
 function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <Footer/>
-    </div>
+
+      <main>
+        <Banner />
+        {accomodationList.map((accomodation) => (
+    <li key={ accomodation.id }>
+      
+    </li>
+))}
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
